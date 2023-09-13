10d12dba99d756c52163204c6feba721801af67e
Jannung Kusdiantoro <iceboltz@gmail.com>
2023-09-13 21:54:03 +0700

    aneh

--- clientapp/src/app/menu/consult/consult-material.module.ts
+++ clientapp/src/app/menu/consult/consult-material.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 const modules = [
   MatIconModule,
   MatFormFieldModule,
   MatInputModule,
-  MatButtonToggleModule
+  MatButtonToggleModule,
+  MatDialogModule,
+  MatSnackBarModule,
+  MatDatepickerModule
 ];
 
 @NgModule({
--- clientapp/src/app/menu/consult/main-frame/main-frame.component.ts
+++ clientapp/src/app/menu/consult/main-frame/main-frame.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { ChatService } from '@app/_services/chat.service';
+import { TicketFormComponent } from '@app/menu/tickets/ticket-form/ticket-form.component';
 
 @Component({
   selector: 'main-frame',
@@ -7,7 +9,23 @@ import { ChatService } from '@app/_services/chat.service';
   styleUrls: ['./main-frame.component.css']
 })
 export class MainFrameComponent {
-  constructor(private chatService: ChatService){
+
+  constructor(private chatService: ChatService, public dialog: MatDialog) {
   
+  }
+
+  openForm(): void {
+    const dialogConfig = new MatDialogConfig();
+    dialogConfig.autoFocus = true;
+    dialogConfig.restoreFocus = true;
+    dialogConfig.minWidth = 400;
+    dialogConfig.minHeight = 480;
+    dialogConfig.maxHeight = 800;
+    dialogConfig.data = {service: this.chatService}
+    const dialogRef = this.dialog.open(TicketFormComponent, dialogConfig);
+    dialogRef.afterClosed().subscribe(result => {
+    }
+
+    );
   }
 }
