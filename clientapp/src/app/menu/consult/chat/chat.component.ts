84b30ade8aac27c716003cf5edceb69386989889
Jannung Kusdiantoro <iceboltz@gmail.com>
2023-09-24 22:35:40 +0700

    besok perbaiki start srtop

--- clientapp/src/app/menu/consult/chat/chat.component.ts
+++ clientapp/src/app/menu/consult/chat/chat.component.ts
@@ -1,95 +1,52 @@
-import { Component, ElementRef, EventEmitter, Input, OnDestroy, Output, ViewChild } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { User } from '@app/_models';
-import { ChatReply, CountdownData, NotificationData, NotificationType, SolveData } from '@app/_models/reply';
+import { ChatReply,SolveData } from '@app/_models/reply';
 import { Ticket } from '@app/_models/ticket';
 import { AuthenticationService } from '@app/_services';
 import { ChatService } from '@app/_services/chat.service';
-import { Subscription, interval } from 'rxjs';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'chat-component',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent implements OnDestroy {
+export class ChatComponent {
+  // input dari parent
   @Input({required: true}) ticketdata!: Ticket;
   @Input({required:true}) isMainRoom:boolean;
+  @Input ({required:true}) counterStart: boolean;
+  @Input ({required:true}) countdownNumber: number;
+  // Output kepada parent
+  @Output() startTrigger = new EventEmitter<{fromserver:boolean, room:string}>();
+  @Output() stopTrigger = new EventEmitter<{fromserver:boolean, room:string}>();
   user: User;
-
-  @ViewChild('triggerbutton') myTriggerButton: ElementRef;
-  @ViewChild('stoptriggerbutton') myStopTriggerButton: ElementRef;
   // untuk notify ke parent
-  @Output() dataemitter: EventEmitter<NotificationData> = new EventEmitter<NotificationData>();
-
   message = new FormControl('');
-  counterStart: boolean = false;
-  countdownNumber: number = 60;
-  private timer: Subscription; 
-
+  
   constructor(private cservice:ChatService, private auService:AuthenticationService) {
     this.auService.user.subscribe(x => {
       this.user = x;
     });
   }
   ngOnInit(){
-    // waiting for trigger countdown
-    this.cservice.getCountDown().subscribe((countDownData:CountdownData) => {
-      console.log(countDownData);
-      if (countDownData.roomId === this.ticketdata.id) {
-        if (countDownData.trigger && !this.counterStart) {
-          this.startCountDown(true);
-        } else if (!countDownData.trigger && this.counterStart){
-          this.stopCountDown(true);
-        }
-      };
-    });
+
   }
 
-  ngOnDestroy(){
-    this.stopCountDown();
-  }
   sendMessage(){
     const cr = <ChatReply>{user:this.user, message:this.message.value, roomId:this.ticketdata.id};
     this.cservice.sendMessage(cr);
     this.message.reset();
   }
 
-  startCountDown(fromserver:boolean = false){
-    if (!this.counterStart) {
-      if (!fromserver) {
-        // emit ke server kalau mulai trigger (room)
-        this.cservice.triggerCountDown(<CountdownData>{ roomId:this.ticketdata.id, trigger:true});
-        // kirim pesan ke pub : memulai trigger
-        this.cservice.sendMessage(<ChatReply>{user:this.user, message:`${this.user.name} memulai trigger close case dan akan tertutup otomatis dalam 60 detik`, roomId:this.ticketdata.id});
-        // console.log(this.user.name, 'start countdown ');
-      }
-      // mulai counter
-      this.counterStart = true;
-      this.timer = interval(1000).subscribe(n => {
-        this.countdownNumber--;
-        if(this.countdownNumber == 0){
-          // selesai, stop counter
-          this.stopCountDown();
-          // approve answer
-          this.approveAnswer();
-        }
-      });
-    }
+  startCountDown(){
+    this.startTrigger.emit({fromserver:false, room:this.ticketdata.id});
+    console.log('chat emit start to main ', this.ticketdata.id);
   }
-  stopCountDown(fromserver:boolean = false){
-    if (this.counterStart) {
-      console.log(this.user.name, ' stop countdown');
-      this.timer.unsubscribe();
-      if (!fromserver) {
-        // kirim pesan ke server untuk menghentikan trigger (room)
-        this.cservice.triggerCountDown(<CountdownData>{roomId:this.ticketdata.id, trigger:false});
-        // notif ke pub kalau trigger dihentikan
-        this.cservice.sendMessage(<ChatReply>{user:this.user, message:`${this.user.name} menghentikan trigger close case`, roomId:this.ticketdata.id});
-      }
-      this.counterStart = false;
-      this.countdownNumber = 60;
-    }
+  stopCountDown(){
+    this.stopTrigger.emit({fromserver:false, room:this.ticketdata.id});
+    console.log('chat emit stop to main ', this.ticketdata.id);
   }
   approveAnswer() {
     // cek jika masih ada counter
@@ -102,12 +59,7 @@ export class ChatComponent implements OnDestroy {
     // approve ke server
      this.cservice.approveAnswer(<SolveData>{solver: this.whosolve(), roomId: this.ticketdata.id});
   }
-  notifyParent(tipe:NotificationType) {
-    console.log('child tries to communicat...');
-    const solver = this.whosolve();
-    const mdt = <NotificationData>{kind:tipe, roomId: this.ticketdata.id, sender: solver};
-    this.dataemitter.emit(mdt);
-  }
+
   whosolve() {
     const replies = [...this.ticketdata.messages].reverse().map(obj => obj.user);
     const solver = replies.find(obj => obj.role !== 'Client');
--- clientapp/src/app/menu/consult/main-frame/main-frame.component.ts
+++ clientapp/src/app/menu/consult/main-frame/main-frame.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, QueryList, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { User } from '@app/_models';
-import { ChatReply, CountdownData, NotificationData, SolveData } from '@app/_models/reply';
+import { ChatReply, CountdownData, SolveData } from '@app/_models/reply';
 import { Ticket } from '@app/_models/ticket';
 import { AuthenticationService, TicketService } from '@app/_services';
 import { ChatService } from '@app/_services/chat.service';
 import { TicketFormComponent } from '@app/menu/consult/ticket-form/ticket-form.component';
+import { ChatComponent } from '../chat/chat.component';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'main-frame',
@@ -18,8 +20,10 @@ export class MainFrameComponent {
   user: User;
   groupToggle = new FormControl('');
   selectedId: string;
-  currentTicket: Ticket;
-  
+  currentTab: TabChat;
+  private defaultCountdown = 60;
+  @ViewChild(ChatComponent) childComponents: QueryList<ChatComponent>;
+
   constructor(private chatService: ChatService,
     private tService: TicketService,
     private authService: AuthenticationService,
@@ -39,7 +43,8 @@ export class MainFrameComponent {
         // this.chatTabs.push(<TabChat>{id:'mainRoom', name: 'Lobi Utama', value: 'mainRoom', hasUpdate: false});
         result['data'].forEach(item => {
           const t = <TabChat>{ id: item.id, name: item.problem,
-                                value: item.name, updateCount: 0, ticket: item};
+                                value: item.name, updateCount: 0, ticket: item, triggerCountdown:false,
+                              timer: null, countDown: this.defaultCountdown };
           if (this.chatTabs.some(obj => obj.id === item.id) === false) {
             if (item.name === 'mainRoom') {
               this.chatTabs.unshift(t);
@@ -50,20 +55,23 @@ export class MainFrameComponent {
           this.chatService.join(item.id);
         });
         this.onSelectionChange();
-        this.currentTicket = this.chatTabs[0].ticket;
+        this.currentTab = this.chatTabs[0];
       //  console.log('tab ada sejumlah: ',this.chatTabs.length);
       });
       // subs for new room
         this.chatService.getRoom().subscribe((roomId) => {
-         console.log(`new room id ${roomId}`);
-        if (this.chatTabs.some(obj => obj['id'] !== roomId)){
-          console.log('no same room id');
-          this.tService.get<Ticket>(roomId).subscribe(result => {
-            this.chatTabs.push({id: result.id, name: result.problem, value: result.name, updateCount: 0, ticket: result, triggerCountdown:false});      
-          });
-        } else {
-          console.log('has same room id');
-        }
+              // console.log(`new room id ${roomId}`);
+              if (this.chatTabs.some(obj => obj.id === roomId)){
+                console.log('has same room');
+                return
+              } else {
+                console.log('no same room');
+                this.tService.get<Ticket>(roomId).subscribe(result => {
+                  this.chatTabs.push(<TabChat>{id: result.id, name: result.problem, value: result.name,
+                                      updateCount: 0, ticket: result, triggerCountdown:false,
+                                      countDown: this.defaultCountdown, timer: null});      
+                });
+              }
         });
           // waiting for new message
     this.chatService.getMessage().subscribe((msg:ChatReply) => {
@@ -76,10 +84,15 @@ export class MainFrameComponent {
     });
         // waiting for trigger countdown
         this.chatService.getCountDown().subscribe((countDownData:CountdownData) => {
-          if (this.chatTabs.some(obj => obj.id === countDownData.roomId) && countDownData.roomId !== this.currentTicket.id) {
+          if (this.chatTabs.some(obj => obj.id === countDownData.roomId) && countDownData.roomId !== this.currentTab.id) {
             const room  = this.chatTabs.find(obj => obj.id === countDownData.roomId);
               room.updateCount++;
               room.triggerCountdown = true;
+              if (countDownData.trigger && !room.triggerCountdown) {
+                this.startCountDown(true, countDownData.roomId);
+              } else if (!countDownData.trigger && room.triggerCountdown) {
+                this.stopCountDown(true, countDownData.roomId);
+              }
           };
         });
       // subs for answer
@@ -110,8 +123,8 @@ export class MainFrameComponent {
     const dialogRef = this.dialog.open(TicketFormComponent, dialogConfig);
     dialogRef.afterClosed().subscribe(result => {
       const room = result.result;
-      this.chatTabs.push({id: room.id, name: room.problem, value: room.name,
-                          updateCount: 0, triggerCountdown: false});
+      this.chatTabs.push(<TabChat>{id: room.id, name: room.problem, value: room.name,
+                          updateCount: 0, triggerCountdown: false, countDown: this.defaultCountdown, timer: Subscription.EMPTY});
       this.chatService.createRoom(room.id);
     });   
   }
@@ -120,16 +133,70 @@ export class MainFrameComponent {
       this.selectedId = event.value;
       // console.log('selectedId ', event.value);
       const tab = this.chatTabs.find(obj => obj.id === this.selectedId);
-      this.currentTicket = tab.ticket;
+      this.currentTab = tab;
       tab.updateCount = 0;
-      tab.triggerCountdown = false;
       // console.log(this.currentTicket);
     } else {
       const tab = this.chatTabs.find(obj => obj.name === 'mainRoom');
-      this.currentTicket = tab.ticket;
+      this.currentTab = tab;
     }
   }
+  startCountDown(fromserver:boolean = false, room: string){
+    const chatRoom = this.chatTabs.find(obj => obj.id === room);
+      if (!fromserver) {
+        // emit ke server kalau mulai trigger (room)
+        this.chatService.triggerCountDown(<CountdownData>{ roomId:room, trigger:true});
+        // kirim pesan ke pub : memulai trigger
+        this.chatService.sendMessage(<ChatReply>{user:this.user, message:`${this.user.name} memulai trigger close case dan akan tertutup otomatis dalam 60 detik`, roomId:room});
+        // console.log(this.user.name, 'start countdown ');
+      }
+    // mulai counter
+    chatRoom.triggerCountdown = true;
+    chatRoom.timer = interval(1000).subscribe(n => {
+      chatRoom.countDown--;
+      if(chatRoom.countDown == 0){
+        // selesai, stop counter
+        this.stopCountDown(false, room);
+        // approve answer
+        this.approveAnswer(chatRoom.id);
+      }
+    });
+}
+childStart(event: {fromserver:boolean, room: string}) {
+  const {fromserver, room} = event;
+  this.startCountDown(fromserver, room);
+}
+childStop(event: {fromserver:boolean, room: string}) {
+  const {fromserver, room} = event;
+  this.stopCountDown(fromserver, room);
+}
+stopCountDown(fromserver:boolean = false, room: string){
+    const chatRoom = this.chatTabs.find(obj => obj.id === room);
+    console.log(this.user.name, ' stop countdown');
+    chatRoom.timer.unsubscribe();
+    if (!fromserver) {
+      // kirim pesan ke server untuk menghentikan trigger (room)
+      this.chatService.triggerCountDown(<CountdownData>{roomId:room, trigger:false});
+      // notif ke pub kalau trigger dihentikan
+      this.chatService.sendMessage(<ChatReply>{user:this.user, message:`${this.user.name} menghentikan trigger close case`, roomId:room});
+    }
+    chatRoom.triggerCountdown = false;
+    chatRoom.countDown = 60;
+}
 
+approveAnswer(room: string) {
+  const child = this.childComponents.find(obj => obj.ticketdata.id === room);
+  const chatRoom = this.chatTabs.find(obj => obj.id === room);
+  // cek jika masih ada counter
+  if (chatRoom.triggerCountdown) {
+    this.stopCountDown(false, chatRoom.id);
+  }
+  // this.notifyParent(NotificationType.newanswer);
+  // kirim pesan ke pub
+  // this.cservice.sendMessage(<ChatReply>{user:this.user, message:`Jawaban diterima`, roomId:this.ticketdata.id});
+  // approve ke server
+   this.chatService.approveAnswer(<SolveData>{solver: child.whosolve(), roomId: room});
+}
 }
 
 export class TabChat {
@@ -138,5 +205,7 @@ export class TabChat {
   value: string;
   updateCount: number;
   triggerCountdown: boolean;
-  ticket?: Ticket;
+  timer: Subscription;
+  countDown: number;
+  ticket: Ticket;
 }
