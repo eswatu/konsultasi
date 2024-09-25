09aa030fadcd70c79042cef31435daba60501501
jk@work <iceboltz@gmail.com>
2024-09-25 16:59:22 +0700

    update sitik auth memory

--- clientapp/src/app/auth/auth.inmemory.service.ts
+++ clientapp/src/app/auth/auth.inmemory.service.ts
@@ -1,4 +1,50 @@
-export class InMemoryAuthService {
+import { Observable, of } from "rxjs";
+import { AuthService, IAuthStatus, IServerAuthResponse } from "./auth.service"
+import { Injectable } from "@angular/core";
+import { User } from "@app/_models";
+import {sign} from 'fake-jwt-sign';
+import { Role } from "./auth.enum";
 
-  constructor() { }
+@Injectable()
+export class InMemoryAuthService extends AuthService {
+  private defaultUser = User.Build({
+    _id: '5da01751da27cc462d265913',
+    username: 'duluca@gmail.com',
+    name: 'Doguhan Uluca' ,
+    role: Role.Admin,
+    company: 'your company',
+    contact: 'contacts',
+    isActive: true
+    })
+  protected override transformJwtToken(token: IAuthStatus): IAuthStatus {
+    return token;
+  }
+  protected override getCurrentuser(): Observable<User> {
+    return of(this.defaultUser)
+  }
+  protected override authProvider(username: string, password: string): Observable<IServerAuthResponse> {
+      username = username.toLowerCase()
+      // validasi username
+      const authStatus = {
+        isAuthenticated: true,
+        userId: this.defaultUser._id,
+        userRole: username.includes('cashier')
+          ? Role.Admin
+          : username.includes('clerk')
+          ? Role.User
+          : Role.None,
+      } as IAuthStatus;
+      this.defaultUser.role = authStatus.userRole
+      const authResponse = {
+        accessToken: sign(authStatus, 'secret', {
+          expiresIn: '1h',
+          algorithm: 'none'
+        }),
+      } as IServerAuthResponse
+      return of(authResponse)
+  }
+  constructor() {
+    super();
+    console.warn("You're using the InMemoryAuthService. Do not use this service in production.")
+  }
 }
--- clientapp/src/app/auth/auth.service.ts
+++ clientapp/src/app/auth/auth.service.ts
@@ -1,14 +1,26 @@
 import { IUser } from "@app/_models/IUser"
-import { BehaviorSubject, Observable } from "rxjs"
+import { BehaviorSubject, catchError, filter, flatMap, map, Observable, tap, throwError } from "rxjs"
 import { Role } from "./auth.enum";
 import { Injectable } from "@angular/core";
 import { User } from "@app/_models";
+import { transformError } from "@app/_helpers/common";
+import { CacheService } from "./cache.service";
+import { jwtDecode } from 'jwt-decode';
 
 export interface IAuthStatus {
   isAuthenticated: boolean;
   userRole: Role;
   userId: string;
 }
+
+export interface IAuthService {
+    readonly authStatus$: BehaviorSubject<IAuthStatus>
+    readonly currentUser$: BehaviorSubject<IUser>
+    login(username: string, password: string): Observable<void>
+    logout(clearToken?: boolean): void
+    getToken(): string
+}
+
 export interface IServerAuthResponse {
   accessToken: string;
 }
@@ -19,31 +31,59 @@ export const defaultAuthStatus: IAuthStatus = {
   userId: ''
 }
 
-export interface IAuthService {
-    readonly authStatus$: BehaviorSubject<IAuthStatus>
-    readonly currentUser$: BehaviorSubject<IUser>
-    login(username: string, password: string): Observable<void>
-    logout(clearToken?: boolean): void
-    getToken(): string
-  }
-
 @Injectable()
-export abstract class AuthService implements IAuthService {
-  protected abstract authProvider(username: string, password: string): Observable<IServerAuthResponse>
-  protected abstract transformJwtToken(token: unknown): IAuthStatus
-  protected abstract getCurrentuser(): Observable<User>
-  
+export abstract class AuthService extends CacheService implements IAuthService {
   readonly authStatus$ = new BehaviorSubject<IAuthStatus>(defaultAuthStatus);
   readonly currentUser$ = new BehaviorSubject<IUser>(new User());
 
+  constructor() {
+    super();
+  }
+  protected abstract authProvider(username: string, password: string): Observable<IServerAuthResponse>
+  protected abstract transformJwtToken(token: unknown): IAuthStatus
+  protected abstract getCurrentuser(): Observable<User>
+
   login(username: string, password: string): Observable<void> {
-    throw new Error("Method not implemented.");
+    this.clearToken()
+    const loginResponse$ = this.authProvider(username, password)
+    .pipe(
+      map((value) => {
+        this.setToken(value.accessToken)
+        const token = jwtDecode(value.accessToken)
+        return this.transformJwtToken(token)
+      }),
+      tap((status) => this.authStatus$.next(status)),
+      filter((status: IAuthStatus) => status.isAuthenticated),
+      flatMap(() => this.getCurrentuser()),
+      map(user => this.currentUser$.next(user)),
+      catchError(transformError)
+    )
+    loginResponse$.subscribe({
+      error: err => {
+        this.logout()
+        return throwError(err)
+      },
+    })
+    return loginResponse$
   }
   logout(clearToken?: boolean): void {
-    throw new Error("Method not implemented.");
+    if (clearToken) {
+      this.clearToken()
+    }
+    setTimeout(() => this.authStatus$.next(defaultAuthStatus),0)
   }
   getToken(): string {
     throw new Error("Method not implemented.");
+  }
+
+  protected setToken(jwt: string) {
+    this.setItem('jwt', jwt)
+  }
+  getTokenSourceMapRange(): string {
+    return this.getItem('jwt') ?? ''
+  }
+  protected clearToken() {
+    this.removeItem('jwt')
   }
 
 }
